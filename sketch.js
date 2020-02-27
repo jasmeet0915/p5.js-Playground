72195ad9f06a4b0a6856bee0be4c2c53da885972
Jasmeet Singh <jasmeet0915@gmail.com>
2020-02-28 00:53:08 +0530

    mic audio input added

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
-
-
+var mic;
 function setup() {
+    createCanvas(200, 200);
+    mic = new p5.AudioIn();
+    mic.start();
 }
 
 function draw() {
+    background(0);
+    var volume = mic.getLevel();
+    ellipse(100, 100, volume*200, volume*200);
+    console.log(volume);
 }
